b3a3734823701d281d7b17b26493e7868380de8d
Andrew <andrewlang.p@gmail.com>
2024-12-09 20:47:29 -0500

    add two instance methods in player to lose life and check life

--- player.rb
+++ player.rb
@@ -1,9 +1,17 @@
 class Player 
   attr_reader :name, :lives
 
-  def initialize(name)
+  def initialize(name, lives = 3)
     @name = name
     @lives = 3
+  end
+  
+  def lose_life
+    @lives -= 1
+  end
+
+  def is_alive?
+    @lives > 0
   end
 end
 
